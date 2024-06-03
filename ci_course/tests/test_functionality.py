427253266fa59a3083e835e49319b0a9b00a91c4
Micah Bowles <micah.r.bowles@gmail.com>
2024-06-03 16:13:17 +0100

    new test for full coverage

--- ci_course/tests/test_functionality.py
+++ ci_course/tests/test_functionality.py
@@ -16,3 +16,6 @@ def test_minimum():
     assert ci_course.minimum(1, 2, 3) == 1
     assert ci_course.minimum(1.2, 2.3) == 1.2
     assert ci_course.minimum(-1.2, -3) == -3
+
+def test_minimum_non_number():
+    assert ci_course.minimum("a", "b") is None
